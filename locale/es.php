d2dae2b7c258372f3b146de7c11f3b43a7f557fe
Domingo Ruiz Arroyo <ordenadordomi@gmail.com>
2022-11-13 13:12:04 +0100

    Update es.php

--- locale/es.php
+++ locale/es.php
@@ -5,6 +5,6 @@
 */
 
 $Definition['Verify registration email'] = 'Verificación de correos electrónicos';
-$Definition['The e-mail domain is not among those allowed.'] = 'El dominio del correo electrónico introduccido no está entre los aceptados.';
+$Definition['The e-mail domain is not among those allowed.'] = 'El dominio del correo electrónico introducido no está entre los aceptados.';
 $Definition['Accepted domain'] = 'Dominios aceptados';
 $Definition['Write the accepted domains separated by commas'] = 'Escribe los dominios aceptados separados por comas';
